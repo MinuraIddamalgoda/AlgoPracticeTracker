17ba3a7b2261b1d6e7ae773a409af5f360104c77
MinuraIddamalgoda <mini2476@gmail.com>
2016-12-29 09:29:45 +1100

    first commit

---
+++ app/src/main/java/com/iddamal/min/algopracticetracker/DialogFragment.java
@@ -0,0 +1,10 @@
+package com.iddamal.min.algopracticetracker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by minIddamal on 29/12/16.
+ */
+
+public class DialogFragment extends Fragment {
+}
