e83e04760b29940cc5f9ca5977e5ebff500ba7ce
MinuraIddamalgoda <mini2476@gmail.com>
2016-12-29 09:40:50 +1100

    adding every other file, something went wrong in adding files in the first two pushes

---
+++ app/src/main/java/com/iddamal/min/algopracticetracker/Question.java
@@ -0,0 +1,40 @@
+package com.iddamal.min.algopracticetracker;
+
+public class Question {
+
+    private int hackerRankCount;
+    private int leetCodeCount;
+    private int ctciCount; // ctci == Cracking The Coding Interview
+
+    public int getHackerRankCount() {
+        return hackerRankCount;
+    }
+
+    public void setHackerRankCount(int hackerRankCount) {
+        this.hackerRankCount = hackerRankCount;
+    }
+
+    public int getLeetCodeCount() {
+        return leetCodeCount;
+    }
+
+    public void setLeetCodeCount(int leetCodeCount) {
+        this.leetCodeCount = leetCodeCount;
+    }
+
+    public int getCtciCount() {
+        return ctciCount;
+    }
+
+    public void setCtciCount(int ctciCount) {
+        this.ctciCount = ctciCount;
+    }
+
+    Question() {
+
+
+    }
+
+}
+
+//                  A Minura Iddamalgoda production                 //
